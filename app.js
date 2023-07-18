f3bac0e0acb759bbf7cc7bcec5993313ced15af0
Shrey Pandey <pandeyshrey45@gmail.com>
2023-07-18 21:32:44 +0530

    third commit

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -19,8 +18,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password :String
 });
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -36,7 +33,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save();
